8f95c9fc00905e48b99152e2ebba78e5e4ac4a72
16aces <zakrohde@gmail.com>
2018-05-17 10:22:48 -0400

    Style updated

---
+++ css/style.js
@@ -0,0 +1,3 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    // Take the user to a different screen here.
+}
