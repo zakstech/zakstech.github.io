572efc653c33cca864bb5a71ba0bdd0e5a2614a3
16aces <zakrohde@gmail.com>
2018-05-17 17:28:19 -0400

    Style updated

--- js/test.js
+++ js/test.js
@@ -2,10 +2,19 @@
 $(document).ready(function() {
      
     // Put your offset checking in a function
-    function checkOffset() {
+   
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  
+    $("#head").removeClass("tall");
+            $("#head").addClass("collapsed");
+            $("#loboBar").css("max-width","80%")
+ function checkOffset() {}
+        }
+    else{
+         function checkOffset() {
 
         if ($("#head").offset().top > 77) {
-        	$("#head").removeClass("tall");
+            $("#head").removeClass("tall");
             $("#head").addClass("collapsed");
 
 
@@ -13,7 +22,7 @@ $(document).ready(function() {
         }     
 
         else if($("#head").offset().top < 20) {
-        	$("#head").addClass("tall");
+            $("#head").addClass("tall");
             $("#head").removeClass("collapsed");
             
         }
@@ -21,12 +30,9 @@ $(document).ready(function() {
 
     // Run it when the page loads
     checkOffset();
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-  
-    TimerSwitch(-100,100);}
-    else{
-      TimerSwitch();  
+       
     }
+    TimerSwitch(); 
 
     // Run function when scrolling
     $(window).scroll(function() {
