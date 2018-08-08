597817780d5be1b14d841deaf1ab92662f146ace
16aces <zakrohde@gmail.com>
2018-08-08 16:41:54 -0400

    better nav bar

---
+++ js/mobile-format.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+     
+    // Put your offset checking in a function
+   
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  
+    $("#head").removeClass("tall");
+            $("#head").addClass("collapsed");
+            $("#loboBar").css("max-width","80%")
+ function checkOffset() {}
+        }
+    else{
+         function checkOffset() {
+
+        if ($("#head").offset().top > 77) {
+            $("#head").removeClass("tall");
+            $("#head").addClass("collapsed");
+
+
+            
+        }     
+
+        else if($("#head").offset().top < 20) {
+            $("#head").addClass("tall");
+            $("#head").removeClass("collapsed");
+            
+        }
+    }
+
+    // Run it when the page loads
+    checkOffset();
+       
+    }
+    TimerSwitch(); 
+
+    // Run function when scrolling
+    $(window).scroll(function() {
+        checkOffset();
+    });
+})
--- js/test.js
+++ js/test.js
@@ -1,44 +1,5 @@
 
-$(document).ready(function() {
-     
-    // Put your offset checking in a function
-   
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-  
-    $("#head").removeClass("tall");
-            $("#head").addClass("collapsed");
-            $("#loboBar").css("max-width","80%")
- function checkOffset() {}
-        }
-    else{
-         function checkOffset() {
 
-        if ($("#head").offset().top > 77) {
-            $("#head").removeClass("tall");
-            $("#head").addClass("collapsed");
-
-
-            
-        }     
-
-        else if($("#head").offset().top < 20) {
-            $("#head").addClass("tall");
-            $("#head").removeClass("collapsed");
-            
-        }
-    }
-
-    // Run it when the page loads
-    checkOffset();
-       
-    }
-    TimerSwitch(); 
-
-    // Run function when scrolling
-    $(window).scroll(function() {
-        checkOffset();
-    });
-})
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
   var scrollBar = {"#scroll1Bar":100, "#scroll2Bar":0, "#scroll3Bar": -100, "#scroll4Bar":-200}
  var reffernce = {"#scroll1Bar":100, "#scroll2Bar":0, "#scroll3Bar": -100, "#scroll4Bar":-200}  
